4eeec3250bd1db8de39852ec40655bd920232354
xptosek <433435@mail.muni.cz>
2016-03-24 22:08:12 +0100

    Vyresetovani implementaco pro misse

--- PV168/src/main/java/pv168/pv168/MissionManagerImpl.java
+++ PV168/src/main/java/pv168/pv168/MissionManagerImpl.java
@@ -1,54 +1,46 @@
 package pv168.pv168;
-
+    
 import java.util.List;
 
 /**
  * Created by Pavel Morcinek on 27.2.2016.
  */
-
 /**
  * Manages mission's CRUD operations
  */
 public class MissionManagerImpl implements MissionManager {
-    /**
-     * creates new mission
-     * @param mission
-     */
+
+//    private static DataSource prepareDataSource() throws SQLException {
+//        EmbeddedDataSource ds = new EmbeddedDataSource();
+//        // we will use in memory database
+//        ds.setDatabaseName("memory:gravemgr-test");
+//        // database is created automatically if it does not exist yet
+//        ds.setCreateDatabase("create");
+//        return ds;
+//    }
+
     @Override
     public void createMission(Mission mission) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    /**
-     * updates existing mission's data
-     * @param mission
-     */
     @Override
     public void updateMission(Mission mission) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    /**
-     * deletes existing mission
-     * @param mission
-     */
     @Override
     public void deleteMission(Mission mission) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    /**
-     * find existing mission by its id
-     * @param id
-     * @return Mission if it exists, otherwise null
-     */
     @Override
     public Mission findMissionById(Long id) {
-        return null;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    /**
-     * @return all existing missions
-     */
     @Override
     public List<Mission> findAllMissions() {
-        return null;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
